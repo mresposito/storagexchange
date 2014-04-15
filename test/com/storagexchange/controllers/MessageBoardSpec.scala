1566cda6edc5d55bc07d4f5a6c5dbfec06907f5f
eskrm <eskrm@yahoo.com>
2014-04-15 13:27:54 -0500

    Fixed message modify tests: parentID/messageID wasn't initialized correctly.

--- test/com/storagexchange/controllers/MessageBoardSpec.scala
+++ test/com/storagexchange/controllers/MessageBoardSpec.scala
@@ -12,10 +12,12 @@ import com.storagexchange.models.MessageStore
 
 trait MessageTest extends Specification {
     
-  val message1 = Message("user1@gmail.com", "user2@yahoo.com", "user1 message to user2", Some(1))
-  val message2 = Message("user2@yahoo.com", "user1@gmail.com", "user2 message to user1", Some(2))
-  val message3 = Message("user2@yahoo.com", "user3@gmail.com", "user2 message to user3", Some(3))
-  val message1Modified = message1.copy(message = "My updated message")
+  val message1 = Message("user1@gmail.com", "user2@yahoo.com", "user1 message to user2")
+  val message2 = Message("user2@yahoo.com", "user1@gmail.com", "user2 reply to user1", Some(1))
+  val message3 = Message("user1@yahoo.com", "user2@gmail.com", "user2 reply to user1", Some(2))
+  val message4 = Message("user2@yahoo.com", "user3@gmail.com", "user2 message to user3")
+  val message1Modified = message1.copy(message = "My updated message", messageID = Some(1))
+  val message2Modified = message2.copy(message = "My updated message", messageID = Some(2))  
 
   val CreateMessages = BeforeHook {
     val Some(create1) = route(createMessageRequest(message1))
@@ -26,7 +28,7 @@ trait MessageTest extends Specification {
   val CreateReplies = BeforeHook {
     val Some(create1) = route(createMessageRequest(message1))
     val Some(create2) = route(createReplyRequest(message2))
-    val Some(create3) = route(createMessageRequest(message3)) 
+    val Some(create3) = route(createReplyRequest(message3))
   }
 
   def createMessageRequest(message: Message) = FakeRequest(POST, "/message").
@@ -72,7 +74,7 @@ class MessageBoardSpec extends Specification with MessageTest {
             }
             "should not display messages between other users" in CreateMessages {
               val Some(myMessages) = route(requestWithSession(routes.MessageBoard.myMessages.url))
-              contentAsString(myMessages) must not contain(message3.message)
+              contentAsString(myMessages) must not contain(message4.message)
             }
             "cannot view my messages if not logged in" in CreateMessages {
               val Some(myMessages) = route(FakeRequest(GET, routes.MessageBoard.myMessages.url))
@@ -81,9 +83,13 @@ class MessageBoardSpec extends Specification with MessageTest {
         }
 
         "Reply to messages" in {
-            "sender can view reply" in CreateReplies {
+            "sender can view 1st reply" in CreateReplies {
               val Some(myMessages) = route(requestWithSession(routes.MessageBoard.myMessages.url))
               contentAsString(myMessages) must contain(message2.message)
+            }
+            "sender can view 2nd reply" in CreateReplies {
+              val Some(myMessages) = route(requestWithSession(routes.MessageBoard.myMessages.url))
+              contentAsString(myMessages) must contain(message3.message)
             }
         }
 
@@ -93,7 +99,7 @@ class MessageBoardSpec extends Specification with MessageTest {
               status(modify) must beEqualTo(BAD_REQUEST)
             }
             "Can't modify a message I don't own" in CreateMessages {
-              val Some(modify) = route(modifyMessage(message2)) 
+              val Some(modify) = route(modifyMessage(message2Modified)) 
               status(modify) must beEqualTo(BAD_REQUEST)
             }
             "modifying my message should be successful" in CreateMessages {
