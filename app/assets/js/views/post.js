23fbcb94b463cdc00455580d0fbb5e8da4814800
eskrm <eskrm@yahoo.com>
2014-04-27 12:36:45 -0500

    Moved map functions to separate require module.

--- app/assets/js/views/post.js
+++ app/assets/js/views/post.js
@@ -2,8 +2,9 @@ define ([
   "jquery",
   "underscore",
   "backbone",
-  "views/post.html"
-], function($, _, Backbone, postHTML) {
+  "views/post.html",
+  "views/map"
+], function($, _, Backbone, postHTML, Map) {
 
   return Backbone.View.extend({
 
@@ -20,23 +21,7 @@ define ([
       this.stepIncrement = 15;
       this.findPosts({});
 
-      if (document.getElementById('map-canvas')) {
-          // Coordinates to center the map. U.S.
-          var latlng = new google.maps.LatLng(37.09024, -95.712891);
-       
-          // By default center map on U.S.
-          var mapOptions = {
-            zoom: 3,
-            center: latlng,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-          };
-       
-          // Attach a map to the DOM Element, with the defined settings
-          window.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-          // Keep track of circles and markers we place on map
-          window.markerArray = [];
-          window.circleArray = [];
-      }
+      Map.initialize()
 
       $(window).scroll(function () {
         if ($(window).scrollTop() >= $(document).height() - $(window).height()) {
@@ -72,7 +57,7 @@ define ([
     },
 
     updateBoard: function() {
-      this.clearOverlays();
+      Map.clearOverlays();
       var query = this.queryValue();
       var filters = this.filterValues();
       var starter = this.starterValues();
@@ -84,63 +69,12 @@ define ([
     universityValues: function() {
       var name = this.$el.find(".universitySearch.tt-input").val();
       if(name.length > 2) {
-        this.panToLocation(name)
+        Map.panToLocation(name)
         return {
           university: name
         }
       } else {
         return {}
-      }
-    },
-
-    panToLocation: function(name) {
-      geocoder = new google.maps.Geocoder();
-      geocoder.geocode( { 'address': name }, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          lat = results[0].geometry.location.lat();
-          lng = results[0].geometry.location.lng();
-          var circle = new google.maps.Circle({
-            center: new google.maps.LatLng(lat, lng),
-            radius: 3000,
-            fillColor: "#FF0000",
-            fillOpacity: 0.3,
-            strokeOpacity: 0.0,
-            strokeWeight: 0,
-            map: window.map
-          });
-          window.circleArray.push(circle);
-          window.map.panTo(new google.maps.LatLng(lat, lng));
-          window.map.fitBounds(circle.getBounds());
-        } 
-      });
-    },
-
-    pinPost: function(post) {
-      if(post["_source"].location !== undefined) {
-        var latlng = post["_source"].location.split(',');
-        var lat = parseFloat(latlng[0]);
-        var lng = parseFloat(latlng[1]);
-        var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(lat, lng),
-          map: window.map
-        });
-        window.markerArray.push(marker);
-      }
-    },
-
-    clearOverlays: function() {
-      if(window.circleArray !== undefined) {
-        for (var i = 0; i < window.circleArray.length; i++) {
-          window.circleArray[i].setMap(null);
-        }
-        window.circleArray.length = 0;
-      }
-
-      if(window.markerArray !== undefined) {
-        for (var i = 0; i < window.markerArray.length; i++) {
-          window.markerArray[i].setMap(null);
-        }
-        window.markerArray.length = 0;
       }
     },
 
@@ -219,7 +153,6 @@ define ([
           var total = data.hits.total;
           self.renderFacets(data.facets.size.ranges);
           self.renderPosts(hits);
-          //41.85,-87.65
         }
       });
     },
@@ -242,7 +175,7 @@ define ([
       }
       var $el = $(this.el);
       var $posts = $el.find(".content .posts");
-      _.map(posts, this.pinPost);
+      _.map(posts, Map.pinPost);
       _.map(posts, function(post) {
         $posts.append(postHTML(post["_source"]));
       });
