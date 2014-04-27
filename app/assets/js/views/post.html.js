60506cf4d67844b477046d27ad0a18d631d45fd4
Michele Esposito <mresposito5@gmail.com>
2014-04-26 20:40:54 -0500

    small style fixes

--- app/assets/js/views/post.html.js
+++ app/assets/js/views/post.html.js
@@ -2,13 +2,13 @@ define([
   "underscore"
 ], function(_) {
   return _.template('\
-    <div class="post well col-md-6"> \
+    <div class="post well col-md-12"> \
       <div class="row"> \
         <div class="col-sm-8"> \
         <b>Description</b><br> \
         <span class="description"><%= description %></span> \
         </div> \
-        <div class="cos-sm-4"> \
+        <div class="cos-sm-2"> \
           <b>Size: </b><span class="size"><%= storageSize %></span> ftsq \
         </div> \
       </div> \
