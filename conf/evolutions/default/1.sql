f36070acd2dd5a289d19e83d1f9d8b07d617228e
Michele Esposito <mresposito5@gmail.com>
2014-04-30 10:49:28 -0500

    remove useless transaction table

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,15 +1,5 @@
 # --- !Ups
 
-CREATE TABLE Transactions (
-  storageTaken INT NOT NULL,
-  fromDate TIMESTAMP NOT NULL,
-  toDate TIMESTAMP NOT NULL,
-  timestamp TIMESTAMP NOT NULL,
-  postingId BIGINT NOT NULL,
-  buyerUserId BIGINT NOT NULL, 
-  fromUserId BIGINT NOT NULL, 
-  id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY);
-
 CREATE TABLE User (
   userID BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
@@ -31,4 +21,3 @@ CREATE VIEW VerifiedUser AS
 
 DROP TABLE User;
 DROP VIEW VerifiedUser;
-DROP TABLE Transactions;
